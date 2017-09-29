d2740a432dc206072072566a8b6b37591865e5ff
lucian95 <luciancondrea95@gmail.com>
2017-09-28 20:09:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome to our website!',
+		anotherMessage: 'The message should be visible'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({errorMessage: 'Error! Computer will self-destruct!'});
+});
+
+app.get('/indirecthelp', (req, res) => {
+	res.sendFile(__dirname + '/public/help.html');
+});
+
+app.listen(3000, () => {console.log('Server started');});

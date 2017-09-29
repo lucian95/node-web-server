75d23b5121bf81be6c908f9dd6eaf21ec17455eb
lucian95 <luciancondrea95@gmail.com>
2017-09-28 21:27:29 -0400

    Added projects page

--- server.js
+++ server.js
@@ -10,19 +10,18 @@ app.set('view engine', 'hbs');
 
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
-hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
-app.use((req, res, next) => {
-	res.render('maintenance.hbs');
-});
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home page',
-		welcomeMessage: 'Welcome to our website!',
-		anotherMessage: 'The message should be visible'
+		welcomeMessage: 'Welcome to our website!'
 	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {pageTitle: 'Projects'});
 });
 
 app.get('/about', (req, res) => {
@@ -31,12 +30,5 @@ app.get('/about', (req, res) => {
 	});
 });
 
-app.get('/bad', (req, res) => {
-	res.send({errorMessage: 'Error! Computer will self-destruct!'});
-});
-
-app.get('/indirecthelp', (req, res) => {
-	res.sendFile(__dirname + '/public/help.html');
-});
 
 app.listen(port, () => {console.log(`Server up on port ${port}`);});
